d5f8fc19287bcf1e1a6825b6d1cfaf1baa08ef21
Julia_Cherkun <cjkunme@gmail.com>
2024-09-09 19:04:54 +0300

    change file js_homework5.2

--- js_homework5.2.js
+++ js_homework5.2.js
@@ -1,18 +1,54 @@
-let anime = {
-  title: "One Piece",
-  author: "Eiichiro Oda",
-  genres: ["fantasy", "adventure"],
-  category: ["shōnen"],
-  
-  getInfo: function() {
-    for (let property in this) {
-      if (this.hasOwnProperty(property) && typeof this[property] !== 'function') {
-        console.log(property + ': ' + this[property]);
+var services = {
+  "стрижка": "60 грн",
+  "гоління": "80 грн",
+  "Миття голови": "100 грн"
+  };
+
+services.price = function() {
+  let sum = 0;
+  for (let property in this) {
+      if (this.hasOwnProperty(property) && typeof this[property] !== 'function'){
+          sum += parseInt(this[property]);
       }
-    }
   }
+  return sum + ' грн';
 };
 
-anime.getInfo()
-anime.episodesCount = 1041;
-anime.getInfo();
+services.minPrice = function() {
+  let min = Infinity;
+  for (let property in this) {
+      if (this.hasOwnProperty(property) && typeof this[property] !== 'function') {
+          let price = parseInt(this[property]);
+          if (price <= min) {
+              min = price;
+          }
+      }
+  }
+  return min + ' грн';
+};
+
+services.maxPrice = function() {
+  let max = 0;
+  for (let property in this) {
+      if (this.hasOwnProperty(property) && typeof this[property] !== 'function') {
+          let price = parseInt(this[property]);
+          if (price >= max) {
+              max = price;
+          }
+      }
+  }
+  return max + ' грн';
+};
+
+
+console.log('Загальна вартість послуг: ' + services.price());
+console.log('Мінімальна вартість послуги: ' + services.minPrice());
+console.log('Максимальна вартість послуги: ' + services.maxPrice());
+
+services['Розбити скло'] = "200 грн";
+
+console.log("ІНФОРМАЦІЯ ПІСЛЯ ДОДАННЯ ПОСЛУГИ");
+
+console.log('Загальна вартість послуг: ' + services.price());
+console.log('Мінімальна вартість послуги: ' + services.minPrice());
+console.log('Максимальна вартість послуги: ' + services.maxPrice());
